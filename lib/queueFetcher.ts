ba93ccaba4b5ffe103e3f7780a4b66cb8c6f0b86
Andreas Eriksson <addeman@gmail.com>
2021-03-18 09:38:54 +0100

    Fix so we do not change child if not needed

--- lib/queueFetcher.ts
+++ lib/queueFetcher.ts
@@ -13,6 +13,8 @@ export default class QueueFetcher {
 
   private changeChildFunc : (childId : string) => Promise<any>
 
+  private lastChildId : string = ''
+
   private scheduleTimeout: any
 
   verboseDebug: boolean = false
@@ -35,7 +37,7 @@ export default class QueueFetcher {
     const promise = queueEntry.queue.enqueue(func, false)
 
     if (this.scheduleTimeout === undefined || this.scheduleTimeout === null) {
-      this.scheduleTimeout = setTimeout(async () => this.schedule(), 2000)
+      this.scheduleTimeout = setTimeout(async () => this.schedule(), 0)
     }
 
     return promise
@@ -54,8 +56,8 @@ export default class QueueFetcher {
       return
     }
 
-    if (this.currentRunningQueue === undefined) {
-      this.debug('First run schedule')
+    if (this.currentRunningQueue === undefined || this.queues.size === 1) {
+      this.debug('First run schedule or only one queue')
       const firstQueue = this.queues.first
       await this.runNext(firstQueue)
       return
@@ -70,7 +72,7 @@ export default class QueueFetcher {
     }
 
     const { id: queueToPauseId, queue: queueToPause } = this.currentRunningQueue
-    this.debug('Queue to pause', queueToPauseId.bold, queueToPause.getQueueInfo())
+    this.debug('Queue to pause', queueToPauseId, queueToPause.getQueueInfo())
 
     queueToPause.pause()
 
@@ -91,9 +93,15 @@ export default class QueueFetcher {
     const { id: childId, queue } = queueToRun
     this.debug('About to run', childId, queue.getQueueInfo())
 
-    this.debug('Initiating change child')
-    await this.changeChildFunc(childId)
-    this.debug('Change child done')
+    if (this.lastChildId === childId) {
+      this.debug('Child already selected, skipping select call')
+    } else {
+      this.debug('Initiating change child')
+      await this.changeChildFunc(childId)
+      this.lastChildId = childId
+      this.debug('Change child done')
+    }
+
     this.currentRunningQueue = queueToRun
 
     this.scheduleTimeout = setTimeout(async () => this.schedule(), 2000)
