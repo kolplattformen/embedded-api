0c97bf4838fe6a8423a0894ed07a96cb3225a9be
Emil Lindqvist <emil.h@bokio.se>
2021-09-19 20:21:28 +0200

    eslint fixes

--- lib/apiGbg.ts
+++ lib/apiGbg.ts
@@ -1,15 +1,42 @@
 import { DateTime, FixedOffsetZone } from 'luxon'
 import { EventEmitter } from 'events'
-import wrap, { Fetcher, FetcherOptions } from './fetcher'
-import { CalendarItem, Classmate, CookieManager, EtjanstChild, Fetch, MenuItem, NewsItem, Notification, ScheduleItem, Skola24Child, TimetableEntry, User } from "./types"
-import { LoginStatusChecker } from './loginStatus'
-import { decode } from 'he'
 import * as html from 'node-html-parser'
+import { decode } from 'he'
+import wrap, { Fetcher, FetcherOptions } from './fetcher'
+import {
+  CalendarItem,
+  Classmate,
+  CookieManager,
+  EtjanstChild,
+  Fetch,
+  MenuItem,
+  NewsItem,
+  Notification,
+  ScheduleItem,
+  Skola24Child,
+  TimetableEntry,
+  User
+} from "./types"
+import { LoginStatusChecker } from './loginStatus'
 import { URLSearchParams } from './URLSearchParams'
 import { IApi } from './IApi'
 import { toMarkdown } from './parseHtml'
 import { HjarntorgetChecker } from './hjarntorgetChecker'
 import { extractInitBankIdParams, extractMvghostRequestBody } from './utils/extractorsGbg'
+
+const checkStatus = (fetch: Fetcher, basePollingUrl: string): LoginStatusChecker =>
+  new HjarntorgetChecker(fetch, basePollingUrl)
+
+function getDateOfISOWeek(week: number, year: number,) {
+  const simple = new Date(year, 0, 1 + (week - 1) * 7)
+  const dow = simple.getDay()
+  const ISOweekStart = simple
+  if (dow <= 4)
+    ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1)
+  else
+    ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay())
+  return ISOweekStart
+}
 
 export class ApiGbg extends EventEmitter implements IApi {
   private fetch: Fetcher
@@ -24,7 +51,6 @@ export class ApiGbg extends EventEmitter implements IApi {
 
   public isFake: boolean = false
 
-
   constructor(
     fetch: Fetch,
     cookieManager: CookieManager,
@@ -35,9 +61,10 @@ export class ApiGbg extends EventEmitter implements IApi {
     this.cookieManager = cookieManager
     this.headers = {}
   }
+
   async getSchedule(child: EtjanstChild, from: DateTime, to: DateTime): Promise<(CalendarItem & ScheduleItem)[]> {
     const lessonsUrl = 'https://hjarntorget.goteborg.se/api/schema/lessons?'
-    var lessonParams = new URLSearchParams({
+    const lessonParams = new URLSearchParams({
       forUser: child.id,
       startDateIso: from.toISODate(),
       endDateIso: to.toISODate(),
@@ -115,17 +142,24 @@ export class ApiGbg extends EventEmitter implements IApi {
       firstName: c.firstName,
       lastName: c.lastName,
       name: `${c.firstName} ${c.lastName}`,
-    } as (Skola24Child & EtjanstChild)));
+    } as (Skola24Child & EtjanstChild)))
   }
 
   async getCalendar(child: EtjanstChild): Promise<CalendarItem[]> {
     const schedule = await this.getSchedule(child, DateTime.now(), DateTime.now().plus({ months: 1 }))
-    return schedule;
+    return schedule
   }
-  getClassmates(_: EtjanstChild): Promise<Classmate[]> {
-    return Promise.resolve([]);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getClassmates(_child: EtjanstChild): Promise<Classmate[]> {
+    if (!this.isLoggedIn) {
+      throw new Error('Not logged in...')
+    }
+    return Promise.resolve([])
   }
-  async getNews(_: EtjanstChild): Promise<NewsItem[]> {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getNews(_child: EtjanstChild): Promise<NewsItem[]> {
     if (!this.isLoggedIn) {
       throw new Error('Not logged in...')
     }
@@ -153,16 +187,21 @@ export class ApiGbg extends EventEmitter implements IApi {
       }
     })
   }
-  async getNewsDetails(child: EtjanstChild, item: NewsItem): Promise<any> {
+
+  async getNewsDetails(_child: EtjanstChild, item: NewsItem): Promise<any> {
     const infoSetReadUrl = `https://hjarntorget.goteborg.se/api/information/set-message-read?messageId=${item.id}`
     this.fetch('info', infoSetReadUrl, {
       method: 'POST',
     })
 
-    return { ...item };
+    return { ...item }
   }
 
-  getMenu(_: EtjanstChild): Promise<MenuItem[]> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getMenu(_child: EtjanstChild): Promise<MenuItem[]> {
+    if (!this.isLoggedIn) {
+      throw new Error('Not logged in...')
+    }
     // Have not found this available on hjärntorget. Perhaps do a mapping to https://www.skolmaten.se/ ?
     return Promise.resolve([])
   }
@@ -193,39 +232,43 @@ export class ApiGbg extends EventEmitter implements IApi {
       .filter(e => e.eventMembers.find(p => p.id === child.id))
       .reduce((acc, e) => acc.concat(e.eventMembers), ([] as any[]))
 
-
     const wallMessagesUrl = 'https://hjarntorget.goteborg.se/api/wall/events?language=en&limit=500'
     const wallMessagesResponse = await this.fetch('wall-events', wallMessagesUrl)
     const wallMessagesResponseJson: any[] = await wallMessagesResponse.json()
-    return wallMessagesResponseJson.filter(message => {
-      return membersInChildsEvents.find(member => member.id === message.creator.id)
-    }).map(message => {
-      const createdDate = new Date(message.created.ts)
-      return {
-        id: message.id,
-        sender: message.creator && `${message.creator.firstName} ${message.creator.lastName}`,
-        dateCreated: createdDate.toISOString(),
-        message: message.body,
-        url: message.url,
-        category: message.title,
-        type: message.type,
-      }
-    });
+    return wallMessagesResponseJson.filter(message =>
+      membersInChildsEvents.find(member => member.id === message.creator.id))
+      .map(message => {
+        const createdDate = new Date(message.created.ts)
+        return {
+          id: message.id,
+          sender: message.creator && `${message.creator.firstName} ${message.creator.lastName}`,
+          dateCreated: createdDate.toISOString(),
+          message: message.body,
+          url: message.url,
+          category: message.title,
+          type: message.type,
+        }
+      })
+  }
 
-  }
   async getSkola24Children(): Promise<(Skola24Child)[]> {
-    return [];
+    if (!this.isLoggedIn) {
+      throw new Error('Not logged in...')
+    }
+    return []
   }
-  async getTimetable(child: Skola24Child, week: number, year: number, _: string): Promise<TimetableEntry[]> {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getTimetable(child: Skola24Child, week: number, year: number, _lang: string): Promise<TimetableEntry[]> {
 
     const startDate = DateTime.fromJSDate(getDateOfISOWeek(week, year))
     const endDate = startDate.plus({ days: 7 })
-    const lessonsUrl = 'https://hjarntorget.goteborg.se/api/schema/lessons?';
-    var lessonParams = new URLSearchParams({
+    const lessonsUrl = 'https://hjarntorget.goteborg.se/api/schema/lessons?'
+    const lessonParams = new URLSearchParams({
       forUser: child.personGuid, // This is a bit of a hack due to how we map things...
       startDateIso: startDate.toISODate(),
       endDateIso: endDate.toISODate(),
-    }).toString();
+    }).toString()
 
     const lessonsResponse = await this.fetch('info', lessonsUrl + lessonParams)
     const lessonsResponseJson: any[] = await lessonsResponse.json()
@@ -251,6 +294,7 @@ export class ApiGbg extends EventEmitter implements IApi {
     })
 
   }
+
   async logout(): Promise<void> {
     this.isLoggedIn = false
     this.cookieManager.clearAll()
@@ -277,8 +321,6 @@ export class ApiGbg extends EventEmitter implements IApi {
     })
 
     const shibbolethRedirectUrl = (shibbolethLoginResponse as any).url
-    const shibbolethLoginResponseText = await shibbolethLoginResponse.text()
-
     const { initBankIdUrl, initBankIdParams } = extractInitBankIdParams(shibbolethRedirectUrl)
     console.log("initiating bankid...")
     const initBankIdResponse = await this.fetch('init-bankId', initBankIdUrl + initBankIdParams, {
@@ -303,20 +345,20 @@ export class ApiGbg extends EventEmitter implements IApi {
     // We may get redirected to some other subdomain i.e. not 'm00-mg-local':
     // https://mNN-mg-local.idp.funktionstjanster.se/mg-local/auth/ccp11/grp/other
     const beingBankIdUrlBase = (mvghostResponse as any).url
-    const beingBankIdUrl = beingBankIdUrlBase + '/ssn'
-    var ssnBody = new URLSearchParams({ ssn: personalNumber }).toString();
-    var beginBankIdResponse = await this.fetch('being-bankid', beingBankIdUrl, {
+    const beingBankIdUrl = `${beingBankIdUrlBase}/ssn`
+    const ssnBody = new URLSearchParams({ ssn: personalNumber }).toString()
+    const beginBankIdResponse = await this.fetch('being-bankid', beingBankIdUrl, {
       redirect: 'follow',
       method: 'POST',
       body: ssnBody,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
-    });
+    })
 
     console.log("start polling")
     // https://mNN-mg-local.idp.funktionstjanster.se/mg-local/auth/ccp11/grp/verify
-    const verifyUrl = (beginBankIdResponse as any).url;
+    const verifyUrl = (beginBankIdResponse as any).url
     const verifyUrlBase = verifyUrl.substring(0, verifyUrl.length - 'verify'.length)
     const statusChecker = checkStatus(this.fetch, verifyUrlBase)
 
@@ -324,27 +366,13 @@ export class ApiGbg extends EventEmitter implements IApi {
       // setting these similar to how the sthlm api does it
       // not sure if it is needed or if the cookies are enough for fetching all info...
       this.isLoggedIn = true
-      this.personalNumber = personalNumber;
+      this.personalNumber = personalNumber
       this.emit('login')
     })
     statusChecker.on('ERROR', () => {
       this.personalNumber = undefined
     })
 
-    return statusChecker;
+    return statusChecker
   }
-}
-
-const checkStatus = (fetch: Fetcher, basePollingUrl: string): LoginStatusChecker =>
-  new HjarntorgetChecker(fetch, basePollingUrl)
-
-function getDateOfISOWeek(week: number, year: number,) {
-  const simple = new Date(year, 0, 1 + (week - 1) * 7);
-  const dow = simple.getDay();
-  const ISOweekStart = simple;
-  if (dow <= 4)
-    ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
-  else
-    ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
-  return ISOweekStart;
 }
--- lib/utils/extractorsGbg.ts
+++ lib/utils/extractorsGbg.ts
@@ -3,7 +3,8 @@ import { decode } from 'he'
 
 // TODO: Move this into the parse folder and convert it to follow the pattern of other parsers (include tests).
 export function extractInitBankIdParams(shibbolethRedirectUrl: any) {
-  const targetParam = decodeURIComponent(shibbolethRedirectUrl.substring(shibbolethRedirectUrl.indexOf('Target=') + 'Target='.length))
+  const targetParamIndex = shibbolethRedirectUrl.indexOf('Target=') + 'Target='.length
+  const targetParam = decodeURIComponent(shibbolethRedirectUrl.substring(targetParamIndex))
   const initBankIdUrl = 'https://auth.goteborg.se/FIM/sps/BankID/saml20/logininitial?'
   const initBankIdParams = new URLSearchParams({
     ITFIM_WAYF_IDP: 'https://m00-mg-local.idp.funktionstjanster.se/samlv2/idp/metadata/0/34',
@@ -13,6 +14,13 @@ export function extractInitBankIdParams(shibbolethRedirectUrl: any) {
     Target: targetParam,
   }).toString()
   return { initBankIdUrl, initBankIdParams }
+}
+
+export const extractInputField = (sought: string, attrs: string[]) => {
+  // there must be a better way to do this...
+  const s = attrs.find(e => e.indexOf(sought) >= 0) || ""
+  const v = s.substring(s.indexOf('value="') + 'value="'.length)
+  return v.substring(0, v.length - 2)
 }
 
 export function extractMvghostRequestBody(initBankIdResponseText: string) {
@@ -25,13 +33,6 @@ export function extractMvghostRequestBody(initBankIdResponseText: string) {
     SAMLRequest: samlRequest 
   }).toString()
   return mvghostRequestBody
-}
-
-export const extractInputField = (sought: string, attrs: string[]) => {
-  // there must be a better way to do this...
-  const s = attrs.find(e => e.indexOf(sought) >= 0) || ""
-  const v = s.substring(s.indexOf('value="') + 'value="'.length)
-  return v.substring(0, v.length - 2)
 }
 
 export function extractHjarntorgetSAMLLogin(authGbgLoginResponseText: string) {
